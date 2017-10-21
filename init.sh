01c032995d626431cd9f3d966d3bce57df86eb4d
Роман Биктайров <biktairov_roman@mail.ru>
2017-10-21 13:27:26 +0300

    2.1.11.v1

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
 sudo /etc/init.d/gunicorn restart
